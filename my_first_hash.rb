c05de393d56cf5fb5627c8ff61c4fbf08efc199d
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-22 23:12:56 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,9 +22,11 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
   }
+
+  shipping_manifest["oil_paintings"]
 end
 
-shipping_manifest["oil paintings"]
+
 
 def adding
   shipping_manifest = {
